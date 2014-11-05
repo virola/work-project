78664f1e8e14df026f185b113d4ccacdcff8e8e1
Virola <virola.zhu@gmail.com>
2014-11-05 18:17:34 +0800

    new

--- gulpfile.js
+++ gulpfile.js
@@ -11,63 +11,76 @@ var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 
-var projects = ['campus-topic'];
-
-var paths = {
-    js:     ['src/*.js'],
-    less:   ['src/css/*.less'],
-    lessDest: ['src/css/main.less'],
-    img:    ['src/']
+var projects = ['campus-pc', 'campus-mobile'];
+var config = {
+    'campus-pc': {
+        dir: 'campus-topic/pc'
+    },
+    'campus-mobile': {
+        dir: 'campus-topic/mobile'
+    }
 };
 
-projects.forEach(function (dir, i) {
-    gulp.task(dir + '-clean', function (cb) {
+var paths = {
+    js:     ['src/js/*.js'],
+    less:   ['src/css/*.less'],
+    lessDest: ['src/css/main.less'],
+    img:    ['src/img/*.png']
+};
+
+projects.forEach(function (project, i) {
+    var dir = config[project].dir;
+    if (!dir) {
+        return false;
+    }
+
+    gulp.task(project + 'clean', function (cb) {
         del([dir + '/asset'], cb);
     });
 
-    gulp.task(dir + '-' + 'js', function() {
+    gulp.task(project + 'js', function() {
         return gulp.src(dir + '/' + paths.js)
             .pipe(concat('main.js'))
             .pipe(gulp.dest(dir + '/' + 'asset/js'));
     });
 
 
-    gulp.task(dir + '-' + 'uglify', [dir + '-' + 'js'], function() {
+    gulp.task(project + 'uglify', [project + 'js'], function() {
         return gulp.src([dir + '/' + 'asset/js/*.js'])
             .pipe(uglify())
             .pipe(gulp.dest(dir + '/' + 'asset/js'));
     });
 
-    gulp.task(dir + '-' + 'img', function() {
+    gulp.task(project + 'img', function() {
         return gulp.src(dir + '/' + paths.img)
             .pipe(imagemin({optimizationLevel: 5}))
             .pipe(gulp.dest(dir + '/' + 'asset/img'));
     });
 
-    gulp.task(dir + '-' + 'less', function () {
+    gulp.task(project + 'less', function () {
         gulp.src(dir + '/' + paths.lessDest)
             .pipe(less({compress: true}))
             .pipe(gulp.dest(dir + '/' + 'asset/css'));
     });
 
-    gulp.task(dir + '-' + 'watch', function() {
-        gulp.watch(dir + '/' + paths.js, [dir + '-' +'js']);
-        gulp.watch(dir + '/' + paths.less, [dir + '-' +'less']);
-        gulp.watch(dir + '/' + paths.img, [dir + '-' +'img']);
+    gulp.task(project + 'watch', function() {
+        gulp.watch(dir + '/' + paths.js, [project +'js']);
+        gulp.watch(dir + '/' + paths.less, [project +'less']);
+        gulp.watch(dir + '/' + paths.img, [project +'img']);
     });
 
-    gulp.task(dir + '-' + 'build', [
-        dir + '-' + 'clean', 
-        dir + '-' + 'uglify', 
-        dir + '-' + 'cssmin', 
-        dir + '-' + 'img'
+    gulp.task(project + 'build', [
+        project + 'clean', 
+        project + 'uglify', 
+        project + 'less', 
+        project + 'img'
     ]);
 });
 
 gulp.task('build', (function () {
     var tasks = [];
     projects.forEach(function (proj, i) {
-        tasks.push(proj + '-' + 'build');
+        tasks.push(proj + 'build');
     });
     return tasks;
 })());
